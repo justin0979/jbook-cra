5c9403b931ad157f1f25ff47c27969e8e4f42001
justin0979 <justinm0979@gmail.com>
2023-07-05 17:17:15 -0500

    setup basic display for testing esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState(""); // transpiled & bundled code to be in the pre element
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <h1>Hi</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        cols={80}
+        rows={15}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };
