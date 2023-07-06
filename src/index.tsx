8357ecc5f5cb4b0811051aa892ad0cf2eeb1dc4a
justin0979 <justinm0979@gmail.com>
2023-07-06 12:11:35 -0500

    setup transpiling of user input code

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,38 @@
 import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom/client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState(""); // transpiled & bundled code to be in the pre element
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: "/esbuild.wasm",
     });
-    console.log(service);
   };
 
   useEffect(() => {
     startService();
   }, []);
 
-  const onClick = () => {
-    console.log(input);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx", // tell which language
+      target: "es2015",
+    });
+
+    setCode(result.code);
   };
 
   return (
