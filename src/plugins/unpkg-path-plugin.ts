efa0aee6b42f094c498da7b2fe512e7c26f5307f
justin0979 <justinm0979@gmail.com>
2023-07-07 13:48:55 -0500

    add if statement to handle './' or '../' paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
@@ -11,7 +12,31 @@ export const unpkgPathPlugin = () => {
        */
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResolve", args);
-        return { path: args.path, namespace: "a" };
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+
+        /*
+         *  handle if args.path = "./utils" or something similar.
+         */
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+
+        // else if (args.path === "tiny-test-pkg") {
+        // return {
+        //   path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+        //   namespace: "a",
+        // };
+        // }
       });
 
       /*
@@ -30,16 +55,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const message = require('medium-test-pkg');
               console.log(message);
               `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: "export default 'hi there!'",
-          };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+        };
       });
     },
   };
