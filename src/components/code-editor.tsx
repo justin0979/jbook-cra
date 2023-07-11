835662246d77d05624a151cede52570d72741d39
justin0979 <justinm0979@gmail.com>
2023-07-11 15:31:30 -0500

    configure editor to pass contents to bundler

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,29 @@
 import MonacoEditor from "@monaco-editor/react";
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => {
+  /*
+   *  1st parameter is a function that returns the contents of the editor.
+   *  2nd parameter is a reference to the editor itself. This can be used to tell the
+   *  editor when the contents of the editor have been changed.
+   */
+  const onEditorDidMount = (
+    getValue: () => string,
+    monacoEditor: any,
+  ) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+
   return (
     <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
       theme="dark"
       language="javascript"
       height="30vh"
