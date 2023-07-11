25eec50fdce1a4f1f28ed6aebd43efda68382eb2
justin0979 <justinm0979@gmail.com>
2023-07-11 18:38:05 -0500

    refactor editor to use onChange prop and refactor App's Editor onChange to check of defined parameter

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,31 +1,20 @@
-import Editor, { OnMount, OnChange } from "@monaco-editor/react";
+import Editor, { OnChange } from "@monaco-editor/react";
 
 interface CodeEditorProps {
   initialValue: string;
-  onChange: (value: string) => void;
+  onChange: OnChange;
 }
 
 const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => {
-  /*
-   *  1st parameter is a function that returns the contents of the editor.
-   *  2nd parameter is a reference to the editor itself. This can be used to tell the
-   *  editor when the contents of the editor have been changed.
-   */
-  const handleChange: OnMount = (editor, monaco) => {
-    editor.onDidChangeModelContent(() => {
-      onChange(editor.getValue());
-    });
-  };
-
   return (
     <Editor
-      //onChange={onChange}
-      onMount={handleChange}
+      onChange={onChange}
       defaultValue={initialValue}
       theme="vs-dark"
-      language="javascript"
+      defaultLanguage="javascript"
       height="30vh"
       options={{
+        tabSize: 2,
         wordWrap: "on",
         minimap: { enabled: false },
         showUnused: false,
