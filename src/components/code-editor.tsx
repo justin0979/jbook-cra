62771c044e26039b651e89a08e6c3ef33bd70cb2
justin0979 <justinm0979@gmail.com>
2023-07-11 15:10:30 -0500

    set basic setup to editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor height="30vh" />;
+  return (
+    <MonacoEditor
+      theme="dark"
+      language="javascript"
+      height="30vh"
+      options={{
+        wordWrap: "on",
+        miniMap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
