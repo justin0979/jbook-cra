d38a8dc6fbf796b0d77e885512125147806da834
justin0979 <justinm0979@gmail.com>
2023-07-13 11:34:40 -0500

    downgrade prettier version, setup basic prettier options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import MonacoEditor, { OnMount, OnChange } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
@@ -8,13 +9,27 @@ interface CodeEditorProps {
 }
 
 const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => {
+  const editorRef = useRef<any>();
+
   const onFormatClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(editorRef.current);
     // get current value from editor
+    const unformatted = editorRef.current.getValue();
+
     // format the value
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
     // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
   };
 
   const handleOnMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
     editor.onDidChangeModelContent(() => {
       onChange(editor.getValue());
     });
