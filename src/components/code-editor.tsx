39f58c910f7f8b306ec6b5c830bf30bd539c146f
justin0979 <justinm0979@gmail.com>
2023-07-13 11:08:16 -0500

    add prettier setup

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,6 @@
-import Editor, { OnChange } from "@monaco-editor/react";
+import MonacoEditor, { OnChange } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,25 +8,35 @@ interface CodeEditorProps {
 }
 
 const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => {
+  const onFormatClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // get current value from editor
+    // format the value
+    // set the formatted value back in the editor
+  };
+
   return (
-    <Editor
-      onChange={onChange}
-      defaultValue={initialValue}
-      theme="vs-dark"
-      defaultLanguage="javascript"
-      height="30vh"
-      options={{
-        tabSize: 2,
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+
+      <MonacoEditor
+        onChange={onChange}
+        defaultValue={initialValue}
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        height="30vh"
+        options={{
+          tabSize: 2,
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
   );
 };
 
