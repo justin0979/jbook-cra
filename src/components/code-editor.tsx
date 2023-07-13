7edfff79cf35df6fb365af11a7388c616856f8dd
justin0979 <justinm0979@gmail.com>
2023-07-13 12:05:16 -0500

    install & hookup bulmaswatch

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -36,7 +36,12 @@ const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => {
 
   return (
     <div>
-      <button onClick={onFormatClick}>Format</button>
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
 
       <MonacoEditor
         onMount={handleOnMount}
