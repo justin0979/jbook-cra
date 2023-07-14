1d1e6d4c539e2da4ebd157d36362da038b2120fd
justin0979 <justinm0979@gmail.com>
2023-07-14 13:22:08 -0500

    refactor Resizable to handle both vertical and horizontal

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,6 +1,6 @@
 import "./resizable.css";
 import { ReactNode } from "react";
-import { ResizableBox } from "react-resizable";
+import { ResizableBoxProps, ResizableBox } from "react-resizable";
 
 interface ResizableProps {
   direction: "horizontal" | "vertical";
@@ -8,17 +8,27 @@ interface ResizableProps {
 }
 
 const Resizable = ({ direction, children }: ResizableProps) => {
-  return (
-    <ResizableBox
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      minConstraints={[10, 100]}
-      height={300}
-      width={Infinity}
-      resizeHandles={["s"]}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ["e"],
+    };
+  } else {
+    resizableProps = {
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [10, 100],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
