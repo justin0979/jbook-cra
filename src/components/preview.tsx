86941137bb361f9260ef5b3633dcfc5068412113
justin0979 <justinm0979@gmail.com>
2023-07-14 16:44:50 -0500

    extract error handling code to separate function in iframe html

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -11,15 +11,18 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
-        window.addEventListener("message", (event) => {
+          const handleError = (err) => {
+            const root = document.querySelector("#root");
+            root.innerHTML = "<div style='color: red;'><h4>Runtime Error</h4>" + err + "</div>";
+            console.error(err);
+          };
+          window.addEventListener("message", (event) => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector("#root");
-              root.innerHTML = "<div style='color: red;'><h4>Runtime Error</h4>" + err + "</div>";
-              console.error(err);
+              handleError(err);
             }
-        }, false);
+          }, false);
         </script>
       </body>
     </html>
