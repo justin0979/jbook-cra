645559f3972fe2e492dae10af8b37e2598852fd5
justin0979 <justinm0979@gmail.com>
2023-07-15 13:25:50 -0500

    add state to handle editor toggling from preview to editor based on where document is clicked

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,39 @@
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 interface TextEditorProps {}
 
 const TextEditor = ({}: TextEditorProps) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener("click", listener, true);
+
+    return () => document.removeEventListener("click", listener, true);
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={"# Header"} />
     </div>
   );
