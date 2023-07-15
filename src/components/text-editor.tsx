98172a02da75dabe280213312ea9d0d8ffeb2c85
justin0979 <justinm0979@gmail.com>
2023-07-15 16:01:52 -0500

    setup temporary component state to hold editor content

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -7,6 +7,7 @@ interface TextEditorProps {}
 const TextEditor = ({}: TextEditorProps) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -28,14 +29,14 @@ const TextEditor = ({}: TextEditorProps) => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
 
   return (
     <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
