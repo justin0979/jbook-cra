5f83a2314988870be7abdfc71723d8c28d7b852e
justin0979 <justinm0979@gmail.com>
2023-07-22 17:34:16 -0500

    define cells reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import { TextEditor } from "./components";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell";
@@ -5,9 +6,9 @@ import { Cell } from "../cell";
 interface CellState {
   loading: boolean;
   error: string | null;
-  order: string[];
+  order: string[]; // array of cell id's, i.e., ["ar1w", "br212", "hij", "z2fe"]
   data: {
-    [key: string]: Cell;
+    [cellId: string]: Cell;
   };
 }
 
@@ -18,23 +19,77 @@ const initialState: CellState = {
   data: {},
 };
 
-const reducer = (
-  state: CellState = initialState,
-  action: Action,
-): CellState => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      return state;
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    default: {
-      return state;
+const reducer = produce(
+  (state: CellState = initialState, action: Action) => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        /*
+         *  below code uses immer
+         */
+        state.data[action.payload.id].content = action.payload.content;
+        return;
+      /*
+       *  below return does not use immer
+       */
+      // return {
+      //   ...state,
+      //   data: {
+      //     ...state.data,
+      //     [action.payload.id]: {
+      //       ...state.data[action.payload.id],
+      //       content: action.payload.content,
+      //     },
+      //   },
+      // };
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return;
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload;
+        const index = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+        const targetIndex = direction === "up" ? index - 1 : index + 1;
+
+        // check to make sure that the targetIndex is within order array bounds
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return;
+        }
+
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+
+        return;
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          content: "",
+          type: action.payload.type,
+          id: randomId(),
+        };
+
+        state.data[cell.id] = cell;
+
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(foundIndex, 0, cell.id);
+        }
+
+        return;
+      default: {
+        return state;
+      }
     }
   }
+);
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
 };
 
 export default reducer;
