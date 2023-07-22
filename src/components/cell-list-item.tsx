1eef7dc9fa473139caab0f0ae2146381679cdcd5
justin0979 <justinm0979@gmail.com>
2023-07-22 18:21:59 -0500

    display CodeCell or TextEditor component based on cell type

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,11 +1,21 @@
+import { ReactNode } from "react";
 import { Cell } from "../state";
+import CodeCell from "./code-cell";
+import TextEditor from "./text-editor";
 
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem = ({ cell }: CellListItemProps) => {
-  return <div>{cell.id}</div>;
+  let child: ReactNode;
+  if (cell.type === "code") {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
