6ba18fa06916915844a4c858fff3088829304eef
justin0979 <justinm0979@gmail.com>
2023-07-23 14:56:04 -0500

    add styling to AddCell

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -9,13 +9,28 @@ const AddCell = ({ nextCellId }: AddCellProps) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div>
-      <button onClick={() => insertCellBefore(nextCellId, "code")}>
-        + Code
-      </button>
-      <button onClick={() => insertCellBefore(nextCellId, "text")}>
-        + Text
-      </button>
+    <div className="add-cell">
+      <div className="add-buttons">
+        <button
+          className="button is-rounded is-primary is-small btn--code"
+          onClick={() => insertCellBefore(nextCellId, "code")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Code</span>
+        </button>
+        <button
+          className="button is-rounded is-primary is-small btn--text"
+          onClick={() => insertCellBefore(nextCellId, "text")}
+        >
+          <span className="icon is-small">
+            <i className="fa fa-plus"></i>
+          </span>
+          <span>Text</span>
+        </button>
+      </div>
+      <div className="divider"></div>
     </div>
   );
 };
