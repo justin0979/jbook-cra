8501805d62e72c71912e1814ed34196dd6a84827
justin0979 <justinm0979@gmail.com>
2023-07-24 09:32:07 -0500

    initial setup of bundlesReducer

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,27 @@
-export const bundlesReducer = () => {};
+import produce from "immer";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+import { stat } from "fs";
+
+interface BundlesState {
+  [cellId: string]: {
+    loading: boolean; // tells if app is currently processing a bundle
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundlesState = {};
+
+export const bundlesReducer = produce(
+  (state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START:
+        return state;
+      case ActionType.BUNDLE_COMPLETE:
+        return state;
+      default:
+        return state;
+    }
+  }
+);
