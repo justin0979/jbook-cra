f24a776a9b151d0b36817e4aa6f3450f8bea25eb
justin0979 <justinm0979@gmail.com>
2023-07-24 09:24:53 -0500

    define bundle actions

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -3,6 +3,21 @@ import { CellTypes } from "../cell";
 
 export type Direction = "up" | "down";
 
+export interface BundleStartAction {
+  type: ActionType.BUNDLE_START;
+  payload: {
+    cellId: string;
+  };
+}
+
+export interface BundleCompleteAction {
+  type: ActionType.BUNDLE_COMPLETE;
+  payload: {
+    cellId: string;
+    bundle: { code: string; err: string };
+  };
+}
+
 export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
@@ -36,4 +51,6 @@ export type Action =
   | MoveCellAction
   | DeleteCellAction
   | InsertCellAfterAction
-  | UpdateCellAction;
+  | UpdateCellAction
+  | BundleStartAction
+  | BundleCompleteAction;
