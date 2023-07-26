0e8365199c950c4aeb08b8f17b8a50fb2e13b06c
justin0979 <justinm0979@gmail.com>
2023-07-26 13:51:06 -0500

    setup fetching cells action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from "redux";
+import axios from "axios";
 import { ActionType } from "../action-types";
 import {
   Action,
@@ -8,7 +9,7 @@ import {
   InsertCellAfterAction,
   Direction,
 } from "../actions";
-import { CellTypes } from "../cell";
+import { Cell, CellTypes } from "../cell";
 import bundle from "../../bundler";
 
 export const updateCell = (
@@ -73,4 +74,26 @@ export const createBundle = (cellId: string, input: string) => async (
       },
     },
   });
+};
+
+export const fetchCells = () => async (dispatch: Dispatch<Action>) => {
+  dispatch({
+    type: ActionType.FETCH_CELLS,
+  });
+
+  try {
+    const { data }: { data: Cell[] } = await axios.get("/cells");
+
+    dispatch({
+      type: ActionType.FETCH_CELLS_COMPLETE,
+      payload: data,
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      dispatch({
+        type: ActionType.FETCH_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
+  }
 };
